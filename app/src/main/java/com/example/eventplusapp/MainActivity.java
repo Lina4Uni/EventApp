dad130208853c991a664e6e8af410b1ac984e659
Lina <lm0ricke@gmail.com>
2024-07-24 19:27:35 +0200

    Initialer Commit

--- app/src/main/java/com/example/eventplusapp/MainActivity.java
+++ app/src/main/java/com/example/eventplusapp/MainActivity.java
@@ -1,71 +1,26 @@
 package com.example.eventplusapp;
 
-
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        return true;
-    }
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        MenuItemType type = MenuItemType.fromId(item.getItemId());
-        switch (type) {
-            case EVENT_MANAGEMENT:
-                // Logik für Eventverwaltung
-                return true;
-            case PARTICIPANT_MANAGEMENT:
-                // Logik für Teilnehmerverwaltung
-                return true;
-            case SCHEDULE_CREATION:
-                // Logik für Zeitplanerstellung
-                return true;
-            case INVITATIONS:
-                // Logik für Einladungen
-                return true;
-            case REMINDERS:
-                // Logik für Erinnerungen
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-}
-
-// Schritt 1: Definieren Sie ein enum für die Menü-IDs
-enum MenuItemType {
-    EVENT_MANAGEMENT(R.id.menu_event_management),
-    PARTICIPANT_MANAGEMENT(R.id.menu_participant_management),
-    SCHEDULE_CREATION(R.id.menu_schedule_creation),
-    INVITATIONS(R.id.menu_invitations),
-    REMINDERS(R.id.menu_reminders),
-    UNKNOWN(0); // Fallback-Wert
-
-    private final int id;
-
-    MenuItemType(int id) {
-        this.id = id;
-    }
-
-    static MenuItemType fromId(int id) {
-        for (MenuItemType type : values()) {
-            if (type.id == id) {
-                return type;
-            }
-        }
-        return UNKNOWN;
+        drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
     }
 }
